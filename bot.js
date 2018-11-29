a3b68f51fd7fcbce6a1138708dc4e2ba71216fe0
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-11-29 06:54:12 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -238,3 +238,19 @@ if(ra3d.content.startsWith(prefix + 'cc')) {
             }
        });
 	
+client.on('guildMemberAdd', member => {
+    let channel = member.guild.channels.find('name', 'welcome');
+    let memberavatar = member.user.avatarURL
+      if (!channel) return; 
+    let embed = new Discord.RichEmbed()
+        .setColor('RANDOM')
+        .setThumbnail(memberavatar)
+        .addField(':running_shirt_with_sash: | name :  ',`${member}`)
+        .addField(':loudspeaker: | نورت السيرفر ي قلبي' , `Welcome to the server, ${member}`)
+        .addField(':id: | user :', "**[" + `${member.id}` + "]**" )
+                .addField('➡| انت العضو رقم',`${member.guild.memberCount}`)
+               
+                  .addField("Name:",`<@` + `${member.id}` + `>`, true)
+                      
+                                     .addField(' الـسيرفر', `${member.guild.name}`,true)
+         });
