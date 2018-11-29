4859339225c5bfa871cb9adc07a5497d4416a524
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-11-29 11:04:46 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -273,3 +273,22 @@ client.on("message", msg => {
       msg.channel.send({embed: embed})
   }
 });
+
+ client.on('message',function(message) {
+        if (message.author.bot) return;
+      var prefix = "!";
+                        if(!message.channel.guild) return;
+      
+                          if (message.content === prefix + "fm") {
+       const embed = new Discord.RichEmbed()
+      
+          .setDescription(`**Members info :sparkles:
+      :green_heart: online:   ${message.guild.members.filter(m=>m.presence.status == 'online').size}
+      :heart:  dnd:       ${message.guild.members.filter(m=>m.presence.status == 'dnd').size}
+      :yellow_heart:  idle:     ${message.guild.members.filter(m=>m.presence.status == 'idle').size}
+      :diamond_shape_with_a_dot_inside:   membersCount:  ${message.guild.memberCount - message.guild.members.filter(m=>m.user.bot).size}
+      :bulb: bots: ${message.guild.members.filter(m=>m.user.bot).size} **`)
+               message.channel.send({embed});
+      
+          }
+            });
