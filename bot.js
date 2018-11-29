4050aab91fbba901d6ff05ba69dfa472ed7e2dc8
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-11-29 05:53:57 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -72,7 +72,7 @@ client.on('message', message => {
 if (message.content.toLowerCase().startsWith(prefix + `new`)) {
     const reason = message.content.split(" ").slice(1).join(" ");
     if (!message.guild.roles.exists("name", "Support Team")) return message.channel.send(`This server doesn't have a \`Support Team\` role made, so the ticket won't be opened.\nIf you are an administrator, make one with that name exactly and give it to users that should be able to see tickets.`);
-    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`You already have a ticket open.`);
+    if (message.guild.channels.exists("name", "ticket-" + message.author.id)) return message.channel.send(`يوجد بالفعل تكت مفتوح.`);
     message.guild.createChannel(`ticket-${message.author.id}`, "text").then(c => {
         let role = message.guild.roles.find("name", "Support Team");
         let role2 = message.guild.roles.find("name", "@everyone");
@@ -91,7 +91,7 @@ if (message.content.toLowerCase().startsWith(prefix + `new`)) {
         message.channel.send(`:white_check_mark: Your ticket has been created, #${c.name}.`);
         const embed = new Discord.RichEmbed()
         .setColor(0xCF40FA)
-        .addField(`Hey ${message.author.username}!`, `Please try explain why you opened this ticket with as much detail as possible. Our **Support Team** will be here soon to help.`)
+        .addField(`Hey ${message.author.username}!`, `نرجو توضيح السبب , سيكون فريق الدعم هنا قريبا لخدمتك.`)
         .setTimestamp();
         c.send({ embed: embed });
     }).catch(console.error);
