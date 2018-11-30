e4394bc5ed248282e26f633d12a05174f6e3d95f
Rejectt <45071435+Rejectt@users.noreply.github.com>
2018-11-30 11:36:10 -0800

    Update bot.js

--- bot.js
+++ bot.js
@@ -4,7 +4,7 @@ const client = new Discord.Client();
 
 
 
-client.login('NTE1OTUxNjAwMTU0NzA1OTYw.Dt8Q9g.YSpfvBnJCe8IoWkIrizmWvBvK6o'); 
+client.login('NTE1OTUxNjAwMTU0NzA1OTYw.DuMilQ.Ep2cuQ6HVUPPABl_ytjnsTz_7K8'); 
 
 
 
